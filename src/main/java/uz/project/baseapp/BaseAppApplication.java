00661f6cfd8f12ed893af67ef90c8c844ee49ce0
turayevhasan <turayevhasan2003@gmail.com>
2025-08-16 09:44:11 +0500

    git init

---
+++ src/main/java/uz/project/baseapp/BaseAppApplication.java
@@ -0,0 +1,13 @@
+package uz.project.baseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/project/baseapp/BaseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.project.baseapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
